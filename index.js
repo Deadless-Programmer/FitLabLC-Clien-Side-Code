faa248921f8830edc8089af7f0c1443f0f368cff
Deadless-Programmer <webtuhin@gmail.com>
2023-06-11 02:46:31 +0600

    get users set

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ async function run() {
     const classCartCollection = client.db("fitLabDB").collection("classCart");
 
 
+
+    app.get('/user', async(req, res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
     app.post('/user', async(req, res)=>{
       const user = req.body;
       const query = { email: user.email }
