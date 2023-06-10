cb2be3c56f6d68083db4f22f261d4b1a2e51b2dd
Deadless-Programmer <webtuhin@gmail.com>
2023-06-10 23:59:03 +0600

    my selected item cart delete code set up

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mimqwr5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -61,10 +61,35 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/classCart', async (req, res) => {
+      const email = req.query.email;
+
+      if (!email) {
+        res.send([]);
+      }
+
+      // const decodedEmail = req.decoded.email;
+      // if (email !== decodedEmail) {
+      //   return res.status(403).send({ error: true, message: 'forbidden access' })
+      // }
+
+      const query = { email: email };
+      const result = await classCartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+
     app.post('/classCart', async (req, res)=>{
       const item =req.body;
       console.log(item);
       const result = await classCartCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.delete('/classCart/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classCartCollection.deleteOne(query);
       res.send(result);
     })
 
